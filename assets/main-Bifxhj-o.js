a934abf0539f8c83dfb82b4108b0a3d091f11706
vadim <144333283+vadyukkha@users.noreply.github.com>
2025-10-30 23:26:31 +0300

    Updates

--- assets/main-Bifxhj-o.js
+++
@@ -1,1 +0,0 @@
-import"./style-CNVNne70.js";console.log("Добро пожаловать в приложение Факты и Картинки!");document.addEventListener("DOMContentLoaded",function(){console.log("Страница загружена!")});
